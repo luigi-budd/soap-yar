ba5862ba186de577bf90191aa805971e67abdea3
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-04-14 17:13:16 -0500

    spin damage also awards amps

--- src/Lua/Character/LUA_soap.lua
+++ src/Lua/Character/LUA_soap.lua
@@ -3318,9 +3318,14 @@ local function try_damage_cases(me,thing, p,soap,DealDamage)
 		return true
 	end
 	
+	local basicdamage = (p.powers[pw_super] or p.powers[pw_invulnerability] or (p.pflags & PF_SPINNING))
+		and not (thing.player and thing.player.valid)
+		and not (thing == me.target or thing == me.tracer)
+	
 	--r-dashing but too slow to deal damage
 	if canBumpAtAll(p)
 		if handleBump(p,me,thing)
+		and not basicdamage
 			return false
 		end
 		return
@@ -3416,9 +3421,7 @@ local function try_damage_cases(me,thing, p,soap,DealDamage)
 	end
 	
 	-- just so you wont miss out on amps
-	if (p.powers[pw_super] or p.powers[pw_invulnerability])
-	and not (thing.player and thing.player.valid)
-	and not (thing == me.target or thing == me.tracer)
+	if basicdamage
 		Soap_ImpactVFX(thing,me, nil, FU/3)
 		Soap_DamageSfx(thing, FU/3, 2*FU)
 		Soap_SpawnBumpSparks(me, thing, nil, true)
