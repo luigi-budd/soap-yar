88b07915d31a06024ec8a4e3db240b0b570aef65
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-07-13 22:11:45 -0500

    Update funcs.lua

--- src/Lua/Debug/funcs.lua
+++ src/Lua/Debug/funcs.lua
@@ -1,3 +1,31 @@
 rawset(_G,"printf",function(fmt,...)
 	print(string.format(fmt,...))
 end)
+
+local MAPBLOCKSHIFT = FRACBITS + 7
+rawset(_G,"visualizeBlockmap",function(mo, fuse, x1,x2, y1,y2)
+	local x1 = (x1 >> MAPBLOCKSHIFT) * 128*FU
+	local x2 = (x2 >> MAPBLOCKSHIFT) * 128*FU
+	local y1 = (y1 >> MAPBLOCKSHIFT) * 128*FU
+	local y2 = (y2 >> MAPBLOCKSHIFT) * 128*FU
+	local pos = {x1,x2, y1,y2}
+	
+	for i = 1, 2
+		for j = 3,4
+			local thisx = pos[i]
+			local thisy = pos[j]
+			local thisz = mo.z
+			
+			for k = -32,32
+				local t = P_SpawnMobj(thisx, thisy, thisz + (4*FU*k), MT_THOK)
+				t.spritexscale = $ / 2
+				t.spriteyscale = t.spritexscale
+				t.color = ColorOpposite(mo.color)
+				t.blendmode = AST_ADD
+				t.tics = fuse
+				t.fuse = -1
+				t.renderflags = $|RF_ALWAYSONTOP|RF_FULLBRIGHT
+			end
+		end
+	end
+end)
